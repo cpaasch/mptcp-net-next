67eba3ff4926aad64ac7d94e535de04b1d604853
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-08-14 22:25:00 +0200

    Cleanup
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1512,8 +1512,6 @@ int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb)
 	if (mopt.drop_me)
 		goto drop;
 #endif
-
-tcp_flow:
 	/* Never answer to SYNs send to broadcast or multicast */
 	if (skb_rtable(skb)->rt_flags & (RTCF_BROADCAST | RTCF_MULTICAST))
 		goto drop;
