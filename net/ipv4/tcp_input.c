0ea49a41e6eb428a45276149fce643fa4df0a0ca
Christoph Paasch <christoph.paasch@uclouvain.be>
2013-12-17 15:55:08 +0100

    Fix: Support mptcp ofo-queue coalescing and collapsing
    When we are under memory-pressure, because the truesize/payload-ratio
    has been underestimated the TCP-stack does ofo-queue coalescing to bring
    the stack back into normal state.
    
    We did not have this implemented for MPTCP.
    This patch supports now ofo-queue coalescing and collapsing of the ofo
    and the receive-queue. The results are really good:
    
    In uml, setting tcp_adv_win_scale to 0, to force an underestimation,
    doing an iperf-session
    
    Before:
    ~250Mbps
    
    After:
    ~600Mbps
    
    And, we collapsed more than 100000 packets:
    TcpExtTCPRcvCollapsed           105252
    
    Reported-by: Nils Caspar <ncaspar@me.com>
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4526,6 +4526,8 @@ static struct sk_buff *tcp_collapse_one(struct sock *sk, struct sk_buff *skb,
 		next = skb_queue_next(list, skb);
 
 	__skb_unlink(skb, list);
+	if (tcp_sk(sk)->mpc)
+		mptcp_remove_shortcuts(tcp_sk(sk)->mpcb, skb);
 	__kfree_skb(skb);
 	NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_TCPRCVCOLLAPSED);
 
@@ -4547,9 +4549,6 @@ tcp_collapse(struct sock *sk, struct sk_buff_head *list,
 {
 	struct sk_buff *skb, *n;
 	bool end_of_skbs;
-
-	if (tcp_sk(sk)->mpc)
-		return;
 
 	/* First, check that queue is collapsible and find
 	 * the point where collapsing can be useful. */
@@ -4656,7 +4655,7 @@ static void tcp_collapse_ofo_queue(struct sock *sk)
 	struct sk_buff *head;
 	u32 start, end;
 
-	if (skb == NULL || tp->mpc)
+	if (skb == NULL)
 		return;
 
 	start = TCP_SKB_CB(skb)->seq;
@@ -4708,7 +4707,7 @@ static bool tcp_prune_ofo_queue(struct sock *sk)
 
 			/* No sack at the mptcp-level */
 			sk_mem_reclaim(sk);
-			res = 1;
+			res = true;
 		}
 		return res;
 	}
